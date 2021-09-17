323fe15b170e3ab8632e312711be94bc63fe53ed
blueudp <blueudp@protonmail.com>
2021-09-17 19:36:11 +0200

    Closes #13; added Target Object

--- deps/fuzzer.py
+++ deps/fuzzer.py
@@ -3,53 +3,45 @@ from colorama import Fore, Style
 
 class Fuzzing:
 
-	def __init__(self, *arg):
-		if len(arg) == 11	:
-			self.URL = arg[0]  # URL to scan
-			self.search_string = arg[1]  # False or string to grep
-			self.notsearch_string = arg[2]  # False or string to inverse grep
-			self.regex = arg[3]  # False or regex to grep
-			self.dont_regex = arg[4] # False or regex to inverse grep
-			self.hidecode = arg[5] # do not show this codes
-			self.showonly = arg[6]  # show only http status code
-			self.usingProxy = arg[7]  # False or COUNTRY to use, NOT proxy
-			self.save = arg[8]
-			self.version = arg[9]
-			self.threads = arg[10]
+	def __init__(self, target):
+
+		if len(dir(target)) == 41:
+			self.target = target 
 			self.banner()
 
 		else:
-			self.parse_config(*arg)
+			self.target = target	
+			self.parse_config(target)
+			self.banner()
 
 	def banner(self):
-		print(f"{Fore.GREEN}Running AF-Team ShellHunt {self.version}{Style.RESET_ALL}")
-		if not self.URL:
-			print(f"\tURLs File:\t{Fore.GREEN}{self.phishings_file}{Style.RESET_ALL}")
+		print(f"{Fore.GREEN}Running AF-Team ShellHunt {self.target.version}{Style.RESET_ALL}")
+		if not self.target.URL:
+			print(f"\tURLs File:\t{Fore.GREEN}{self.target.phishings_file}{Style.RESET_ALL}")
 		else:
-			print(f"\tURL:\t{Fore.GREEN}{self.URL}{Style.RESET_ALL}")
+			print(f"\tURL:\t{Fore.GREEN}{self.target.URL}{Style.RESET_ALL}")
 
-		if self.save:
-			print(f"\tSaving to:\t{Fore.GREEN}{self.save}{Style.RESET_ALL}")
+		if self.target.save:
+			print(f"\tSaving to:\t{Fore.GREEN}{self.target.save}{Style.RESET_ALL}")
 
-		if self.hidecode:
-			print(f"\tNot showing\t{Fore.RED}{str(self.hidecode)[1:-1]}{Style.RESET_ALL}")
-		if self.showonly:
-			print(f"\tShowing only\t{Fore.GREEN}{str(self.showonly)[1:-1] }{Style.RESET_ALL}")
-		print(f"\tThreads:\t{Fore.GREEN}{self.threads}{Style.RESET_ALL}")
+		if self.target.hidecode:
+			print(f"\tNot showing\t{Fore.RED}{str(self.target.hidecode)[1:-1]}{Style.RESET_ALL}")
+		if self.target.showonly:
+			print(f"\tShowing only\t{Fore.GREEN}{str(self.target.showonly)[1:-1] }{Style.RESET_ALL}")
+		print(f"\tThreads:\t{Fore.GREEN}{self.target.threads}{Style.RESET_ALL}")
 
-		if self.search_string:
-			print(f"\tShowing only coincidence with:\t{Fore.GREEN}{self.search_string}{Style.RESET_ALL}")
-		if self.notsearch_string:
-			print(f"\tNot showing coincidence with:\t{Fore.RED}{self.notsearch_string}{Style.RESET_ALL}")
-		if self.regex:
-			print(f"\tShowing only coincidence with:\t{Fore.GREEN}{self.regex}{Style.RESET_ALL}")
-		if self.dont_regex:
-			print(f"\tNot showing coincidence with:\t{Fore.RED}{self.dont_regex}{Style.RESET_ALL}")
+		if self.target.search_string:
+			print(f"\tShowing only coincidence with:\t{Fore.GREEN}{self.target.search_string}{Style.RESET_ALL}")
+		if self.target.donotsearch_string:
+			print(f"\tNot showing coincidence with:\t{Fore.RED}{self.target.donotsearch_string}{Style.RESET_ALL}")
+		if self.target.regex:
+			print(f"\tShowing only coincidence with:\t{Fore.GREEN}{self.target.regex}{Style.RESET_ALL}")
+		if self.target.dont_regex:
+			print(f"\tNot showing coincidence with:\t{Fore.RED}{self.target.dont_regex}{Style.RESET_ALL}")
 
 
-	def parse_config(self, *args):
-		self.version = args[-1]
-		self.banner()
+	def parse_config(self, object):
+		pass
 
 	def find_shell(self): # Fuzz URL and Filter w/ passed arguments hc,hs, threads...
 		pass
--- deps/scanner.py
+++ deps/scanner.py
@@ -2,30 +2,36 @@ from colorama import Fore, Style
 import configparser
 from deps.fuzzer import Fuzzing
 
-class scanner:
-	def __init__(self,version, URL, File, Save, threads, hidecode, showonly, proxy_country, filterbystring, notshowstring, filterbyregex, notshowregex):
-
-		self.URL = URL  # URL ( when using --urproxy_country)
-		self.usingProxy=proxy_country
-		self.phishings_file = File  # URL (when using --file)
-		self.save = Save  #  output
-		self.threads = threads  # number of threads
+class Target:
+	def __init__(self, args):
+		#  URL, File, Save, threads, hidecode, showonly, proxy_country, filterbystring, notshowstring, filterbyregex, notshowregex
+		
+		self.version = args[0]
+		self.URL = args[1]  # URL ( when using --urproxy_country)
+		self.phishings_file = args[2]  # URL (when using --file)
+		self.save = args[3]  #  output
+		self.threads = args[4]  # number of threads
+		self.hidecode =  args[5]  # do not show response w/ this http codes
+		self.showonly =  args[6]  # show response w/ this http codes
+		self.usingProxy= args[7]
+		self.search_string =  args[8]
+		self.donotsearch_string =  args[9]
+		self.regex =  args[10]
+		self.dont_regex =  args[11]	
 		self.headers = []  # here will be UA loadaded from config
-		self.countries = {}  # proxy list {"country":["proxy1"...]}
-		self.hidecode = hidecode  # do not show response w/ this http codes
-		self.showonly = showonly  # show response w/ this http codes
-		self.version = version
-		self.search_string = filterbystring
-		self.donotsearch_string = notshowstring
-		self.regex = filterbyregex
-		self.dont_regex = notshowregex
-		if File:  # if user passed a file...
+		self.countries = {}  # proxy list {"country":["proxy1"...]}	
+		
+		if self.phishings_file:  # if user passed a file...
 			config = configparser.RawConfigParser()
-			config.read(File)
+			config.read(self.phishings_file)
 			self.phishing_list = config.items() # load sites from user file, separated by countries ( to use proxy )
 			self.urls_object = config  # for fuzzer
 		else:
 			self.phishing_list = False  # 4 line 71 check
+
+class scanner:
+	def __init__(self, *args):
+		self.target = Target(args)
 
 
 	def parse_config(self):
@@ -37,35 +43,33 @@ class scanner:
 			print(e)
 			exit(1)
 
-		self.countries = dict(config.items('PROXIES'))  # read proxys from config
-		if self.usingProxy:
-			print(f"\tProxy:\t{Fore.GREEN}{str(self.usingProxy)}{Style.RESET_ALL}")
+		self.target.countries = dict(config.items('PROXIES'))  # read proxys from config
+		if self.target.usingProxy:
+			print(f"\tProxy:\t{Fore.GREEN}{str(self.target.usingProxy)}{Style.RESET_ALL}")
 
-		if self.phishing_list:  # prints used proxies when ph file loaded
+		if self.target.phishing_list:  # prints used proxies when ph file loaded
 			print_countries = []
-			for i in self.phishing_list:
+			for i in self.target.phishing_list:
 				if i[0] != "DEFAULT":
 					print_countries.append(i[0])
 			print(f"\tProxy:\t{Fore.GREEN}{str(print_countries)[1:-1]}{Style.RESET_ALL}")	
 
-		if self.usingProxy and self.usingProxy not in self.countries:  # exit if country not in config file
+		if self.target.usingProxy and self.target.usingProxy not in self.target.countries:  # exit if country not in config file
 			print(f"\n{Fore.RED}the country is not in the conf file!{Style.RESET_ALL}")
 			exit(1)
 
-		if len(self.countries):
-			for key in self.countries:
-				self.countries[key] = self.countries[key].split(',')  # return {"country": ["proxies"]}
-		self.headers = dict(config.items('HEADERS'))  # read HEADERS from config
+		if len(self.target.countries):
+			for key in self.target.countries:
+				self.target.countries[key] = self.target.countries[key].split(',')  # return {"country": ["proxies"]}
+		self.target.headers = dict(config.items('HEADERS'))  # read HEADERS from config
 
 	def start(self):
 		self.parse_config()
 
-		if self.URL:  # do pass  self.headers too!!!
-			fuzz = Fuzzing(self.URL, self.search_string, self.donotsearch_string, self.regex, self.dont_regex, self.hidecode, self.showonly, self.usingProxy, self.save, self.version, self.threads)
+		if self.target.URL:  # do pass  self.headers too!!!
+			fuzz = Fuzzing(self.target)
 			fuzz.find_shell()
-			"""
-			overload -> url to scan, show match, do not show match, show regex match, do not show regex match, do not show results w/ status code, show only w/ this codes, proxy country
-			 """
+
 		else:
-			fuzz = Fuzzing(self.urls_object, self.save, self.version)  # constructor overload not allowed in python
+			fuzz = Fuzzing(self.target)  # constructor overload not allowed in python
 			fuzz.find_shell()
