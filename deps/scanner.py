3e31230c817c26bafb7c86009c4939c6879e138d
blueudp <blueudp@protonmail.com>
2021-09-17 03:14:05 +0200

    Closes #5, #4, #1; Programmed parsers

--- deps/scanner.py
+++ deps/scanner.py
@@ -1,17 +1,22 @@
 from colorama import Fore, Style
+import configparser
 
 class scanner:
-	def __init__(self, URL, File, Save, threads):
-		self.URL = URL
-		self.phishings_file = File
-		self.save = Save
-		self.threads = threads
-
+	def __init__(self,version, URL, File, Save, threads, hidecode, showonly):
+		self.URL = URL  # URL ( when using --url)
+		self.phishings_file = File  # URL (when using --file)
+		self.save = Save  #  output
+		self.threads = threads  # number of threads
+		self.UA = ""  # here will be UA loadaded from config
+		self.countries = {}  # proxy list {"country":["proxy1"...]}
+		self.hidecode = hidecode  # do not show response w/ this http codes
+		self.showonly = showonly  # show response w/ this http codes
+		self.version = version
 		if File:
 			self.url_dict = {}  # loaded from file
 
 	def banner(self):
-		print(f"{Fore.GREEN}Running AFTeam ShellHunt{Style.RESET_ALL}")
+		print(f"{Fore.GREEN}Running AFTeam ShellHunt {self.version}{Style.RESET_ALL}")
 		if self.phishings_file:
 			print(f"\tLoaded {Fore.GREEN}{self.phishings_file}{Style.RESET_ALL}")
 		else:
@@ -20,8 +25,28 @@ class scanner:
 		if self.save:
 			print(f"\tSaving to {Fore.GREEN}{self.save}{Style.RESET_ALL}")
 
+		if self.hidecode:
+			print(f"\tNot showing responses with HTTP status {Fore.RED}{self.hidecode}{Style.RESET_ALL}")
+		if self.showonly:
+			print(f"\tShowing only responses with HTTP status {Fore.GREEN}{self.showonly}{Style.RESET_ALL}")
 		print(f"\tRunning {Fore.RED}{self.threads}{Style.RESET_ALL} threads")
 
+	def parse_config(self):
+		config = configparser.RawConfigParser()
+		try:
+			config.read("deps/config.txt")
+		except Exception as e:
+			print(f"{Fore.RED}Corrupted config file!{Style.RESET_ALL}")
+			print(e)
+			exit(1)
+
+		self.countries = dict(config.items('PROXIES'))  # read proxys from config
+
+		if len(self.countries):
+			for key in self.countries:
+				self.countries[key] = self.countries[key].split(',')  # return {"country": ["proxies"]}
+				# random.choice(self.countries[country])
 
 	def start(self):
 		self.banner()
+		self.parse_config()
--- shellfinder.py
+++ shellfinder.py
@@ -2,25 +2,27 @@
 import argparse
 from deps.scanner import scanner
 
-__version__ = "1.0.1"
+__version__ = "1.0.1b"
 
 
-def main():
+def main(version):
 	parser = argparse.ArgumentParser()
 	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument('--url', '-u', action='store', dest='URL', help='URL to scan')
 	group.add_argument('--file', '-f', action='store', dest='File',help='Phishings file', default=False)
+
 	parser.add_argument('--save', '-s', action='store', dest='Save', help='Save to', default=False)
 	parser.add_argument('--threads', '-t', action='store', dest='threads', help='Threads to run, default 20', default=20)
-
+	parser.add_argument('--hide-code', '-hc', action='store', dest='hidecode', help='Do not show responses w/ this code',nargs="+", type=int, default=[])
+	parser.add_argument('--show-code', '-sc', action='store', dest='showonly', help='Do not show responses w/o this code',nargs="+", type=int, default=[200,302])
 	results = parser.parse_args()
 
 	if not results.URL and not results.File:
 		print("You have to input some URL")
-
-	scan = scanner(results.URL, results.File, results.Save, results.threads)
+	scan = scanner(version, results.URL, results.File, results.Save, results.threads,
+		results.hidecode, results.showonly)
 
 	scan.start()
 
 if __name__=="__main__":
-	main()
+	main(__version__)
